1103151f12042839e23fc3e6560d13390e2e366b
Sulav Baral <sulavbaral58@gmail.com>
2022-10-04 09:23:17 +0545

    changes

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(express.json()); //req.body
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
 }
-app.use(express.static(path.join(__dirname, "client/build")));
 
 //ROUTES//
 
