9b37dee292496d58c250b639908c1436aebe743c
Sulav Baral <sulavbaral58@gmail.com>
2022-10-07 20:28:05 +0545

    color fixes

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="max-w-full bg-green-600 border-gray-200 px-4 py-6 rounded dark:bg-gray-900">
+            <nav className="max-w-full bg-green-600 border-gray-200 px-4 py-6 rounded">
                 <div className="flex flex-wrap justify-between items-center">
                     <a href="/" className="flex items-center ">
                         <img src={Logo} className="mr-3 h-6 sm:h-9" alt="something" />
@@ -48,7 +48,7 @@ const Navbar = () => {
                                 </li>
                             </ul>
                         </div> */}
-                        <button onClick={handleNav} type="button" className="inline-flex items-center ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                        <button onClick={handleNav} type="button" className="inline-flex items-center ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                         </button>
--- client/src/Components/UpdateTodo.jsx
+++ client/src/Components/UpdateTodo.jsx
@@ -33,7 +33,7 @@ function UpdateTodo(props) {
 
     return (
         <>
-            <button onClick={handleShow} className="block text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="authentication-modal">
+            <button onClick={handleShow} className="block text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" data-modal-toggle="authentication-modal">
                 Edit
             </button>
 
